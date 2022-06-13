b17a53c2070fd71785ab846a98d360f7c464f37a
monte <pooroldmichaelfinnegan@gmail.com>
2022-06-14 02:13:58 +1200

    fixed chunk loop a bit, added more chunks, saved some chunk-specific json files for gl

--- p3d2json/chunks.py
+++ p3d2json/chunks.py
@@ -8,14 +8,53 @@ def P3D(block): return {}
 CHUNKS = { b'\x50\x33\x44\xFF': P3D }
 
 
-# fence walls
+# fence walls idk dsg
 def FenceDSG(block): return {}
 def Wall(block):
     _start = struct.unpack('<3f',block[:0xC])
     _end = struct.unpack('<3f',block[0xC:0x18])
     _normal = struct.unpack('<3f',block[0x18:0x24])
     return {'Start':{'X':_start[0],'Y':_start[1],'Z':_start[2]},'End':{'X':_end[0],'Y':_end[1],'Z':_end[2]},'Normal':{'X':_normal[0],'Y':_normal[1],'Z':_normal[2]}}
-CHUNKS = { **CHUNKS, **{ b'\x07\x00\xF0\x03': FenceDSG , b'\x00\x00\x00\x03': Wall }}
+# CHUNKS = { **CHUNKS, **{ b'\x07\x00\xF0\x03': FenceDSG, b'\x00\x00\x00\x03': Wall }}
+
+
+def IntersectDSG(block):
+    _num_indices, = struct.unpack('<I', block[:0x4])
+    # _indices = struct.unpack(f'<{_num_indices}I', block[0x4:0x4+_num_indices*0x4])
+    _indices = [struct.unpack('<3I',block[i:i+0xC]) for i in range(0x4,0x4+_num_indices*0x4,0xC)]
+    _num_positions, = struct.unpack('<I', block[0x4+_num_indices*0x4:0x4+_num_indices*0x4+0x4])
+    # _positions = struct.unpack(f'<{_num_positions*3}f', block[0x4+_num_indices*0x4+0x4:0x4+_num_indices*0x4+0x4+_num_positions*0xC])
+    _positions = [struct.unpack(f'3f', block[i:i+0xC]) for i in range(0x4+_num_indices*0x4+0x4,0x4+_num_indices*0x4+0x4+_num_positions*0xC,0xC)]
+    _num_normals, = struct.unpack('<I', block[0x4+_num_indices*0x4+0x4+_num_positions*0xC:0x4+_num_indices*0x4+0x4+_num_positions*0xC+0x4])
+    # _normals = struct.unpack(f'{_num_normals*3}f', block[0x4+_num_indices*0x4+0x4+_num_positions*0xC+0x4:0x4+_num_indices*0x4+0x4+_num_positions*0xC+0x4+_num_normals*0xC])
+    _normals = [struct.unpack(f'3f', block[i:i+0xC]) for i in range(0x4+_num_indices*0x4+0x4+_num_positions*0xC+0x4,0x4+_num_indices*0x4+0x4+_num_positions*0xC+0x4+_num_normals*0xC,0xC)]
+    return { 'Indices': _indices, 'Positions': _positions, 'Normals': _normals }
+def BBox(block):
+    _box = struct.unpack('6f' ,block[:0x1C])
+    return { 'Box': _box }
+def BSphere(block):
+    _sphere = struct.unpack('4f', block[:0x1C])
+    return { 'Sphere': _sphere }
+def TerrainType(block):
+    _version, = struct.unpack('<I', block[:0x4])
+    _num_types, = struct.unpack('<I', block[0x4:0x8])
+    _types = struct.unpack(f'{_num_types}B', block[0x8:0x8+_num_types])
+    return { 'Version': _version, 'Types': _types }
+CHUNKS = { **CHUNKS, **{ b'\x03\x00\xF0\x03': IntersectDSG, b'\x03\x00\x01\x00': BBox, b'\x04\x00\x01\x00': BSphere, b'\x0E\x00\x00\x03': TerrainType }}
+
+
+def TreeDSG(block):
+    _num_nodes, = struct.unpack('<I', block[:0x4])
+    _bounds_min = struct.unpack('3f', block[0x4:0x10])
+    _bounds_max = struct.unpack('3f', block[0x10:0x1C])
+    return { 'NumNodes': _num_nodes, 'BoundsMin': _bounds_min, 'BoundsMax': _bounds_max }
+def ContiguousBinNode(block):
+    _sub_tree_size, _parent_offset = struct.unpack('<2I', block[:0x8])
+    return { 'SubTreeSize': _sub_tree_size, 'ParentOffset':_parent_offset }
+def SpatialNode(block):
+    _plane_axis, _plane_posn, _num_s_entities, _num_s_phys, _num_intersects, _num_d_phys, _num_fences, _num_roads, _num_paths, _num_anims = struct.unpack('<Bf8I', block[:0x37])
+    return { 'PlaneAxis': _plane_axis, 'PlanePosn': _plane_posn, 'NumSEntities': _num_s_entities, 'NumSPhys': _num_s_phys, 'NumIntersects': _num_intersects, 'NumDPhys': _num_d_phys, 'NumFences': _num_fences, 'NumRoads': _num_roads, 'NumPaths': _num_paths, 'NumAnims': _num_anims }
+# CHUNKS = { **CHUNKS, **{ b'\x04\x00\xF0\x03': TreeDSG, b'\x05\x00\xF0\x03': ContiguousBinNode, b'\x06\x00\xF0\x03': SpatialNode }}
 
 
 # locators
@@ -40,7 +79,7 @@ def WBTriggerVolume(block):
     _scale = struct.unpack(f'3f', block[_name_size+0x5:_name_size+0x11])
     _matrix = struct.unpack(f'16f', block[_name_size+0x11:_name_size+0x51])
     return { 'Name': _name, 'Type': _type, 'Scale': _scale, 'Matrix': _matrix }
-CHUNKS = { **CHUNKS, **{ b'\x05\x00\x00\x03': WBLocator, b'\x06\x00\x00\x03': WBTriggerVolume }}
+# CHUNKS = { **CHUNKS, **{ b'\x05\x00\x00\x03': WBLocator, b'\x06\x00\x00\x03': WBTriggerVolume }}
 
 
 # textures & images  # todo
@@ -56,16 +95,49 @@ CHUNKS = { **CHUNKS, **{ b'\x05\x00\x00\x03': WBLocator, b'\x06\x00\x00\x03': WB
 # CHUNKS = { **CHUNKS, **{ b'\x00\x90\x01\x00': Texture, b'\x01\x90\x01\x00': Image, b'\x02\x90\x01\x00': ImageData, b'\x03\x90\x01\x00': ImageFileName, b'\x04\x90\x01\x00': VolumeImage, b'\x05\x90\x01\x00': Sprite }}
 
 
-# collision  # todo
-def CollisionObject(block): return {}
-def CollisionObjectAttribute(block): return {}
-def CollisionVolumeOwner(block): return {}
-def CollisionVolumeOwnerName(block): return {}
-def SelfCollision(block): return {}
-def CollisionVolume(block): return {}
-def BBoxVolume(block): return {}
-def SphereVolume(block): return {}
-def CylinderVolume(block): return {}
-def OBBoxVolume(block): return {}
+# collision
+def StaticPhysDSG(block):
+    _name_size = block[0]
+    _name = struct.unpack(f'{_name_size}s', block[0x1:0x1+_name_size])[0].decode('ascii').strip('\x00')
+    _version, = struct.unpack(f'<I', block[0x1+_name_size:0x1+_name_size+4])
+    return { "Name": _name, "Version": _version }
+def CollisionObject(block):
+    _name_size = block[0]
+    _name = struct.unpack(f'{_name_size}s', block[0x1:0x1+_name_size])[0].decode('ascii').strip('\x00')
+    _version, = struct.unpack(f'<I', block[0x1+_name_size:0x1+_name_size+4])
+    return { 'Name': _name, "Version": _version }
+def CollisionObjectAttribute(block):
+    _static_attribute, _default_area, _can_roll, _can_slide, _can_spin, can_bounce, _extra_attribute_1, _extra_attribute_2, _extra_attribute_3 = struct.unpack('<HI4H3I', block[:0x20])
+    return {'StaticAttribute': _static_attribute, 'CanRoll': _default_area, 'CanSlide': _can_roll, 'CanSpin': _can_slide, 'CanBounce': can_bounce, 'ExtraAttribute1': _extra_attribute_1, 'ExtraAttribute2': _extra_attribute_2, 'ExtraAttribute3': _extra_attribute_3 }
+def CollisionVolumeOwner(block):
+    _num_names, = struct.unpack(f'<I', block[:0x4])
+    return { 'NumNames': _num_names }
+def CollisionVolumeOwnerName(block):
+    _name_size = block[0]
+    _name = struct.unpack(f'{_name_size}s', block[0x1:0x1+_name_size])[0].decode('ascii').strip('\x00')
+    return { 'Name': _name }
+def SelfCollision(block):
+    _joint_index_1, _joint_index2, _self_only_1, _self_only_2 = struct.unpack('<2I2H', block[:0xC])
+    return { 'JointIndex1': _joint_index_1, 'JointIndex2': _joint_index2, 'SelfOnly1': _self_only_1, 'SelfOnly2': _self_only_2 }
+def CollisionVolume(block):
+    _object_reference_index, = struct.unpack(f'<I', block[:0x4])
+    _owner_index, = struct.unpack(f'<I', block[0x4:0x8])
+    _num_sub_volume, = struct.unpack(f'<I', block[0x8:0xC])
+    return { "ObjectReferenceIndex": _object_reference_index, "OwnerIndex": _owner_index, "NumSubVolume": _num_sub_volume }
+def BBoxVolume(block):
+    _nothing, = struct.unpack(f'<I', block[:0x4])
+    return { 'Nothing': _nothing }
+def SphereVolume(block):
+    _sphere_radius, = struct.unpack(f'f', block[:0x4])
+    return { 'SphereRadius': _sphere_radius }
+def CylinderVolume(block):
+    _cylinder_radius, _length, _flat_end = struct.unpack(f'<2fH', block[:0x10])
+    return { 'CylinderRadius': _cylinder_radius, 'Length': _length, 'FlatEnd': _flat_end }
+def OBBoxVolume(block):
+    _length_1, _length_2, _length_3 = struct.unpack('3f', block[:0xC])
+    return { 'Length1': _length_1, 'Length2': _length_2, 'Length3': _length_3 }
 def WallVolume(block): return {}
-def CollisionVector(block): return {}
+def CollisionVector(block):
+    _x, _y, _z = struct.unpack('3f', block[:0xC])
+    return { 'X': _x, 'Y': _y, 'Z': _z }
+# CHUNKS = { **CHUNKS, **{ b'\x01\x00\xF0\x03': StaticPhysDSG, b'\x00\x00\x01\x07': CollisionObject, b'\x23\x00\x01\x07': CollisionObjectAttribute, b'\x21\x00\x01\x07': CollisionVolumeOwner, b'\x22\x00\x01\x07': CollisionVolumeOwnerName, b'\x20\x00\x01\x07': SelfCollision, b'\x01\x00\x01\x07': CollisionVolume, b'\x06\x00\x01\x07': BBoxVolume, b'\x02\x00\x01\x07': SphereVolume, b'\x03\x00\x01\x07': CylinderVolume, b'\x04\x00\x01\x07': OBBoxVolume, b'\x05\x00\x01\x07': WallVolume, b'\x07\x00\x01\x07': CollisionVector }}
--- p3d2json/main.py
+++ p3d2json/main.py
@@ -5,10 +5,12 @@ import json
 from chunks import *
 from list_of_chunk_ids import chunk_id_list  # for hacky chunk counter
 
-# with open('./p3ds/flandersHouse.p3d', 'rb') as file:
-# with open('./p3ds/l4/loadZones.p3d', 'rb') as file:
-# with open('./p3ds/l4/sr1_.p3d', 'rb') as file:
-with open('./p3ds/L1_TERRA.p3d', 'rb') as file:
+# with open('./p3ds/flandersHouse.p3d', 'rb') as file:  # modified p3ds to test on
+# with open('./p3ds/l4/loadZones.p3d', 'rb') as file:   # 
+# with open('./p3ds/l4/sr1_.p3d', 'rb') as file:        # 
+
+# with open('./p3ds/L1_TERRA.p3d', 'rb') as file:
+with open('./p3ds/l1z1.p3d', 'rb') as file:
     p3d_file = file.read()
 
 counter = {}
@@ -22,38 +24,47 @@ def _next(block):
     # child_size = int.from_bytes(block[data_size+0x8:data_size+0xC], 'little')
 
     child_list, _sum = [], 0
-    while data_size+_sum < chunk_size:  # child_list
+    while data_size+_sum < chunk_size:
         # print(chunk_id, data_size + _sum)
         child_size = int.from_bytes(block[data_size+_sum+0x8:data_size+_sum+0xC], 'little')
         child_list += [_next(block[data_size+_sum:])]  # append child_list
         _sum += child_size  # move to next child
 
-    if chunk_id not in counter: counter[chunk_id] = 1
-    else: counter[chunk_id] += 1
+    # chunk_raw_data = block[header:data_size].decode('utf-8', 'replace')
 
-    if chunk_id not in CHUNKS and chunk_id in chunk_id_list:
-        return chunk_id_list[chunk_id]
+    temp_list = []
+    if child_list:
+        for v in child_list:
+            if v: temp_list += [v]
+    child_list = temp_list
 
-    # chunk_data = CHUNKS[chunk_id](block[header:data_size])    
-    chunk_data = [chunk_id_list[chunk_id], CHUNKS[chunk_id](block[header:data_size])][chunk_id in CHUNKS]
+    name = ''
+    if chunk_id not in CHUNKS:
+        chunk_data = {};return {}
+        # if chunk_id in chunk_id_list: name = chunk_id.decode('ascii', 'replace')
+        # else: name = chunk_id_list[chunk_id]
+    else:
+        name = CHUNKS[chunk_id].__name__
+        chunk_data = CHUNKS[chunk_id](block[header:data_size])
 
+        # print(name)
+        # json.dumps(chunk_data)  # catch non json compatible types
 
 
     if chunk_data and child_list:
-        return {CHUNKS[chunk_id].__name__: [chunk_data, child_list]}
+        return { name: [chunk_data, child_list] }
     elif not chunk_data and child_list:
-        return {CHUNKS[chunk_id].__name__: child_list}
+        return { name: child_list }
     elif chunk_data and not child_list:
-        return {CHUNKS[chunk_id].__name__: chunk_data}
-    else:
-        return {}
+        return { name: chunk_data }
+    else: return name
 
 
 if __name__ == '__main__':
     ret = _next(p3d_file)
-    print(ret)
+    # print(ret)
     with open('./dump.json', 'wt') as dump:
         json.dump(ret, dump, indent=2)
     # for i in counter:
-        # if i in chunk_id_list: print(counter[i], chunk_id_list[i])
-        # else: print(counter[i], i)
+    #     if i in chunk_id_list: print(counter[i], chunk_id_list[i])
+    #     else: print(counter[i], i)
